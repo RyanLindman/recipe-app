e8a8e74860736a8b1d030b92111aad66d8394e51
Ryan Lindman <ryan.lindman@outlook.com>
2023-11-09 12:07:44 +0100

    added some navigation

--- frontend/src/views/Recipe.tsx
+++ frontend/src/views/Recipe.tsx
@@ -1,10 +1,13 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Header from "../components/Header";
 import recipe_data from "../recipe_data.json";
+import { useNavigate } from "react-router-dom";
+
 import "./Recipe.css";
 
 const Recipe = () => {
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
 
   const recipe = recipe_data.find(
     (item) => item.id === parseInt(id as string, 10)
@@ -23,26 +26,28 @@ const Recipe = () => {
       <Header fontSize={18} />
 
       <div id="recipe-card-id">
-        <div className="recipe-id-header">
-          
-          
-        </div>
+        <div className="recipe-id-header"></div>
         <div className="recipe-read">
-          <h2 style={{ fontWeight: 200 }}>{recipe.name}</h2>
-          <h4>{recipe.description}</h4>
+          <div className="title-box">
+            <div>
+              <h2 style={{ fontWeight: 200 }}>{recipe.name}</h2>
+              <h4>{recipe.description}</h4>
+            </div>
+            <Link to={""} onClick={() => navigate(-1)} className="goback-btn">
+              Go back
+            </Link>
+          </div>
 
-          
           <img src={recipe.ImagePath} alt="recipe-img" />
 
           <div className="ingredients">
-            
-            <p style={{ fontWeight: 600 }}>Ingredients</p>
+            <p style={{ fontWeight: 600 }}>Ingredients:</p>
             <br />
             {recipe.ingredients.map((item, index) => (
               <li key={index}>{item}</li>
             ))}
             <br />
-            
+
             <div className="estimated">
               <p style={{ fontWeight: 600 }}>Estimated time:</p>
               <p>{recipe.EstimatedCookingTime}</p>
@@ -50,12 +55,14 @@ const Recipe = () => {
           </div>
           <hr />
           <div className="recipe-inst">
-            <p style={{fontWeight:600, fontSize:"larger"}}>Do like this: </p>
+            <p style={{ fontWeight: 600, fontSize: "larger" }}>
+              Do like this:{" "}
+            </p>
             <p>{recipe.instructions}</p>
           </div>
         </div>
       </div>
-      
+
       <div className="space"></div>
     </div>
   );
